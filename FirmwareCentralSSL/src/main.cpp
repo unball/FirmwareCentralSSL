13b14a40fab62f5484e7a44432ae6a194a749e9a
brunobash <brunov.eng@gmail.com>
2024-10-28 22:57:31 -0300

    firmware ESP Central

--- FirmwareCentralSSL/src/main.cpp
+++ FirmwareCentralSSL/src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "Wire.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define I2C_DEV_ADDR 0x55
+
+uint32_t i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Wire.begin();  // Inicia comunicação I2C
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(5000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Envia mensagem para o periférico
+  Wire.beginTransmission(I2C_DEV_ADDR);
+  Wire.printf("Hello World! %lu", i++);
+  uint8_t error = Wire.endTransmission(true);
+  Serial.printf("Fim da transmissão: %u\n", error);
+
+  // Lê 16 bytes do periférico
+  uint8_t bytesReceived = Wire.requestFrom(I2C_DEV_ADDR, 16);
+  Serial.printf("Bytes recebidos: %u\n", bytesReceived);
+
+  if (bytesReceived > 0) {
+    uint8_t temp[bytesReceived];
+    Wire.readBytes(temp, bytesReceived);
+    log_print_buf(temp, bytesReceived);  // Exibe buffer
+  }
 }
