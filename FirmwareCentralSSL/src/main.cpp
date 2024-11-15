d4c72d24e2f497a394ca3992deabdba5c3757cd0
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-11-15 14:48:49 -0300

    Revert "adapta codigo para receber velocidades do wifi"
    This reverts commit 1b94bd7daa9452c20204dcac56ec5dafa175f0d9.

--- FirmwareCentralSSL/src/main.cpp
+++ FirmwareCentralSSL/src/main.cpp
@@ -1,7 +1,6 @@
 #include "Arduino.h"
 #include "Wire.h"
 #include "Mestre.hpp"
-#include "wifi.hpp"
 
 #define I2C_DEV_ADDR 0x55
 
@@ -35,28 +34,20 @@ void setup()
   Mestre::setup();
   // Inicia comunicação I2C
   // TODO: Fazer setup do WiFi
-  Wifi::setup();
 }
 
 void loop()
 {
-  int16_t vx_int = 0;
-  int16_t vy_int = 0;
-  int16_t w_int = 0;
-  // Lê velocidades pelo Wifi
-  Wifi::receiveData(&vx_int, &vx_int, &w_int);
+  // TODO: Remover delay
+  delay(5000);
+
+  // TODO: Recebe velocidades x,y e w do wifi
 
   // TODO: Traduz velocidades x, y e w para velocidade das rodas u1,u2,u3 e u4
-  std::array<float, 4> speeds_result = calc_speed_2_motor(vx_int, vy_int, w_int);
-  float u1 = speeds_result[0];
-  float u2 = speeds_result[1];
-  float u3 = speeds_result[2];
-  float u4 = speeds_result[3];
-
+  //  calc_speed_2_motor(colocar argumentos)
 
   // TODO: Envia velocidade das rodas para drivers
-  Mestre::send_speed_2_driver(addr_driver1, u1, u2);
-  Mestre::send_speed_2_driver(addr_driver2, u3, u4);
+  //  send_speed_2_drivers(float* goal_velocity_motor)
 
   // TODO: Colocar comunicação abaixo na função send_speed_2_drivers
   //  Envia mensagem para o periférico
@@ -78,34 +69,34 @@ void loop()
   }*/
 
   // valores arbitários
-  // float x_dot = 0.0;
-  // float y_dot = 0.0;
-  // float theta_dot = 0.0;
+  float x_dot = 0.0;
+  float y_dot = 0.0;
+  float theta_dot = 0.0;
 
-  // // supostamente faz um quadrado
-  // float dir_x[] = {1,
-  //                  0,
-  //                  -1,
-  //                  0};
+  // supostamente faz um quadrado
+  float dir_x[] = {1,
+                   0,
+                   -1,
+                   0};
 
-  // float dir_y[] = {0,
-  //                  1,
-  //                  0,
-  //                  -1};
+  float dir_y[] = {0,
+                   1,
+                   0,
+                   -1};
 
-  // for (int i = 0; i < 4; i++)
-  // {
-  //   std::array<float, 4> speeds_result = calc_speed_2_motor(dir_x[i], dir_y[i], theta_dot);
-  //   float u1 = speeds_result[0];
-  //   float u2 = speeds_result[1];
-  //   float u3 = speeds_result[2];
-  //   float u4 = speeds_result[3];
+  for (int i = 0; i < 4; i++)
+  {
+    std::array<float, 4> speeds_result = calc_speed_2_motor(dir_x[i], dir_y[i], theta_dot);
+    float u1 = speeds_result[0];
+    float u2 = speeds_result[1];
+    float u3 = speeds_result[2];
+    float u4 = speeds_result[3];
 
-  //   Mestre::send_speed_2_driver(addr_driver1, u1, u2);
-  //   Mestre::send_speed_2_driver(addr_driver2, u3, u4);
+    Mestre::send_speed_2_driver(addr_driver1, u1, u2);
+    Mestre::send_speed_2_driver(addr_driver2, u3, u4);
 
-  //   delay(1500);
-  // }
+    delay(1500);
+  }
 
   // Serial.printf("x_dot: %f, y_dot: %f, theta_dot: %f\n", x_dot, y_dot, theta_dot);
   // Serial.printf("u1 (depois): %f, u2 (depois): %f\n", u1, u2);
