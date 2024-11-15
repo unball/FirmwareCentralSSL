f3399d6dea7a7a29b1d7644dc5f62f06f7904cdb
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-11-15 15:01:25 -0300

    mensagem struct

---
+++ FirmwareCentralSSL/lib/wifi/wifi.cpp
@@ -0,0 +1,75 @@
+#include "wifi.hpp"
+
+
+/* Wi-Fi */ 
+const int communicationTimeout = 500000;
+const int resetTimeout = 4000000;
+const float MAX_POWER = 10.0;
+
+namespace Wifi{
+
+    uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+    rcv_message temp_msg;
+    rcv_message msg;
+
+    volatile static uint32_t lastReceived;
+
+    uint8_t robotNumber;
+
+    void setup(){
+
+        WiFi.mode(WIFI_STA);
+        WiFi.disconnect();
+        if (esp_now_init() != ESP_OK) {
+            #if WEMOS_DEBUG
+            Serial.println("Erro ao inicializar o ESP-NOW");
+            #endif
+            return;
+        }
+        WiFi.setTxPower(WIFI_POWER_19_5dBm);
+        esp_err_t error = esp_wifi_set_channel(14, WIFI_SECOND_CHAN_NONE);
+
+        esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));            
+    }
+
+    // Callback function, execute when message is received via Wi-Fi
+    void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len){
+        memcpy(&temp_msg, incomingData, sizeof(temp_msg));
+	    lastReceived = micros();
+
+        if(temp_msg.checksum == temp_msg.vx + temp_msg.vy + temp_msg.w){
+            msg = temp_msg;
+        }
+        else{
+            #if WEMOS_DEBUG
+                Serial.println("###################");
+		        Serial.println("###################");
+                Serial.println("ERRO DE CHECKSUM");
+                Serial.println("###################");
+                Serial.println("###################");
+            #endif
+        }
+        // TODO: lastReceived deveria estar aqui ou em receiveData?
+    }
+    /// @brief Receive data copying from temp struct to global struct
+    /// @param v reference to the linear velocity
+    /// @param w reference to the angular velocity
+    void receiveData(int16_t *vx, int16_t *vy, int16_t *w){
+            // Demultiplexing and decoding the velocities and constants
+            *vx  = msg.vx;
+            *vy  = msg.vy;
+            *w  = msg.w;
+    }
+
+    bool isCommunicationLost(){
+        if((micros() - lastReceived) > communicationTimeout){
+			// Communication probably failed
+			if((micros() - lastReceived) > resetTimeout)
+				ESP.restart();
+            return true;
+		}
+        return false;
+    }
+
+}
---
+++ FirmwareCentralSSL/lib/wifi/wifi.hpp
@@ -0,0 +1,28 @@
+#ifndef WIFI_H
+#define WIFI_H
+#include <WiFi.h>
+#include <esp_now.h>
+#include <cstdlib>
+#include <cstring>
+#include <esp_wifi.h>
+
+#include "../../include/config.h"
+
+namespace Wifi{
+
+    struct rcv_message{
+        int8_t id;
+        float vx;
+        float vy;
+        float w;
+        int32_t checksum;
+    };
+  
+    void setup();
+    void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
+    void receiveData(int16_t *vx, int16_t *vy, int16_t *w);
+    bool isCommunicationLost();
+    void tokenize(const uint8_t *data,int len);
+}
+
+#endif
